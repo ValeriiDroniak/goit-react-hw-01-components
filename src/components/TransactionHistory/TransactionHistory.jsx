687cddd3abcba01cb218f657464c1f2e2aca3af5
ValeriiDroniak <105050454+ValeriiDroniak@users.noreply.github.com>
2022-12-04 23:47:04 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,8 +28,6 @@ export const TransactionHistory = ({ items }) => {
           );
         })}
       </tbody>
-
-      {/* <TransactionHistoryBody items={items} /> */}
     </TableTransaction>
   );
 };
