bcdbd2b2c872c01e5346b983c9fbd7d56ca9c672
Valerii Droniak <droniak.valerii@gmail.com>
2022-12-05 00:03:50 +0200

    transformation

--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -1,29 +1,30 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Friend = styled.li`
-    display: flex;
-    min-width: 240px;
-    justify-content: left;
-    align-items: center;
-    gap: 16px;
-    padding: 16px;
-    background-color: rgb(255, 255, 255);
-    box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
+  display: flex;
+  min-width: 240px;
+  justify-content: left;
+  align-items: center;
+  gap: 16px;
+  padding: 16px;
+  background-color: rgb(255, 255, 255);
+  box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px,
+    rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
 `;
 
 export const Status = styled.span`
-        width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    background-color: ${({ isOnline }) => isOnline ? "green" : "red"};
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background-color: ${({ isOnline }) => (isOnline ? 'green' : 'red')};
 `;
 
 export const Avatar = styled.img`
-    width: 48px;
-    height: 48px;
+  width: 48px;
+  height: 48px;
 `;
 
 export const Name = styled.p`
-    font-weight: bold;
-    color: rgb(60, 71, 86);
+  font-weight: bold;
+  color: rgb(60, 71, 86);
 `;
